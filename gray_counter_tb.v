2b2f76a69b670e51ff2eb2a3cc78f4b04d506f72
Atmadip <52615332+Atmadip@users.noreply.github.com>
2024-05-05 18:06:57 -0500

    Update gray_counter_tb.v

--- gray_counter_tb.v
+++ gray_counter_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/18/2024 11:21:24 AM
-// Design Name: 
-// Module Name: gray_counter_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module gray_counter_tb #(parameter data_width = 4);
 
